aabc374770e4bea6bee8c18fe171a95f082e1b16
lava.matteo <lava.matteo.06@itisdalmine.edu.it>
2023-10-18 20:25:24 +0200

    aggiunti i compiti per il 19-10-23

---
+++ 19_10/Televisione/Televisione/Class1.cs
@@ -0,0 +1,153 @@
+﻿namespace Televisione
+{
+    public class Televisione
+    {
+        public string marca;
+        public string modello;
+        public int numeroMassimoCanali = 100;
+        public int VolumeMinimo = 0;
+        public int VolumeMassimo = 50;
+        public bool aColori;
+
+        private bool accesa = false;
+        private int canale = 0;
+        private int volume;
+
+
+        /*costruttore con tutte le variabili*/
+        public Televisione(string marca, string modello, int numeroMassimoCanali, int VolumeMinimo, int VolumeMassimo, bool aColori)
+        {
+            this.marca = marca;
+            this.modello = modello;
+            this.numeroMassimoCanali = numeroMassimoCanali;
+            this.VolumeMinimo = VolumeMinimo;
+            this.VolumeMassimo = VolumeMassimo;
+            this.aColori = aColori;
+
+            volume = VolumeMinimo;
+    }
+
+        /*costruttore con solo le variabili essenziali*/
+        public Televisione(string marca, string modello, bool aColori)
+        {
+            this.marca = marca;
+            this.modello = modello;
+            this.aColori = aColori;
+
+            volume = VolumeMinimo;
+        }
+
+        public void accendi()
+        {
+            accesa = true;
+        }
+
+        public void spegni()
+        {
+            accesa = false;
+        }
+        public void alzaVolume()
+        {
+            if (accesa == true)
+            {
+                if (volume < VolumeMassimo)
+                {
+                    volume++;
+                }
+            }
+        }
+        public void abbassaVolume()
+        {
+            if (accesa == true)
+            {
+                if (volume > VolumeMinimo)
+                {
+                    volume--;
+                }
+            }
+        }
+        public void alzaVolume(int n)
+        {
+            if (accesa == true)
+            {
+                if (volume + n < VolumeMassimo)
+                {
+                    volume = volume + n;
+                }
+                else
+                {
+                    volume = VolumeMassimo;
+                }
+            }
+        }
+        public void abbassaVolume(int n)
+        {
+            if (accesa == true)
+            {
+                if (volume - n > VolumeMinimo)
+                {
+                    volume = volume - n;
+                }
+                else
+                {
+                    volume = VolumeMinimo;
+                }
+            }
+        }
+
+        public void incrementaCanale()
+        {
+            if (accesa == true)
+            {
+                if (canale < numeroMassimoCanali)
+                {
+                    canale++;
+                }
+            }
+        }
+        public void decrementaCanale()
+        {
+            if (accesa == true)
+            {
+                if (canale > 0)
+                {
+                    canale--;
+                }
+            }
+        }
+
+        public bool isAccesa()
+        {
+            return accesa;
+        }
+
+        public int canaleCorrente()
+        {
+            return canale;
+        }
+
+        public int volumeCorrente()
+        {
+            return volume;
+        }
+
+        public string informazioni()
+        {
+            return $"{marca}, {modello}, {numeroMassimoCanali}, {VolumeMinimo}, {VolumeMassimo}, {aColori},";
+        }
+
+        public void copiaInfoTelevisione(Televisione daSovrascrivere)
+        {
+            daSovrascrivere.marca = this.marca;
+            daSovrascrivere.modello = this.modello;
+            daSovrascrivere.numeroMassimoCanali = this.numeroMassimoCanali;
+            daSovrascrivere.VolumeMinimo = this.VolumeMinimo;
+            daSovrascrivere.VolumeMassimo = this.VolumeMassimo;
+            daSovrascrivere.aColori = this.aColori;
+
+            daSovrascrivere.accesa = this.accesa;
+            daSovrascrivere.canale = this.canale;
+            daSovrascrivere.volume = this.volume;
+        }
+    }
+}
---
+++ 19_10/Televisione/Televisione/Program.cs
@@ -0,0 +1,65 @@
+﻿namespace Televisione
+{
+    public class Programma
+    {
+        public static void Main(string[] args)
+        {
+            Televisione Sony;
+            Sony = new Televisione("Sony", "BRAVIA KD-32W800", 200, 0, 100, true);
+
+            Televisione Mivar;
+            Mivar = new Televisione("Mivar", "17BN2L", false);
+
+            Televisione Philips;
+            Philips = new Televisione("Philips", "32PFL5403D", 70, 0, 75, true);
+
+            Console.WriteLine($"{Sony.marca} - {Mivar.modello} - {Philips.aColori} - {Mivar.numeroMassimoCanali}");
+
+            Sony.accendi();
+            Console.WriteLine(Sony.isAccesa());
+
+            Console.WriteLine(Sony.volumeCorrente());
+            Sony.alzaVolume(60);
+            Console.WriteLine(Sony.volumeCorrente());
+
+            Sony.abbassaVolume();
+            Console.WriteLine(Sony.volumeCorrente());
+
+            Sony.alzaVolume(60);
+            Console.WriteLine(Sony.volumeCorrente());
+
+            Sony.abbassaVolume(120);
+            Console.WriteLine(Sony.volumeCorrente());
+
+            Sony.alzaVolume();
+            Console.WriteLine(Sony.volumeCorrente());
+
+            Sony.spegni();
+            Console.WriteLine(Sony.isAccesa());
+
+            Console.WriteLine(Sony.informazioni());
+
+            Sony.accendi();
+
+            Console.WriteLine(Sony.canaleCorrente());
+            Sony.incrementaCanale();
+            Console.WriteLine(Sony.canaleCorrente());
+
+            Sony.decrementaCanale();
+            Console.WriteLine(Sony.canaleCorrente());
+
+            Sony.decrementaCanale();
+            Console.WriteLine(Sony.canaleCorrente());
+
+            Mivar.copiaInfoTelevisione(Sony);
+            Console.WriteLine(Sony.informazioni());
+
+            Console.WriteLine(Sony.isAccesa());
+            Sony.accendi();
+            Console.WriteLine(Sony.isAccesa());
+
+            Sony.alzaVolume(60);
+            Console.WriteLine(Sony.volumeCorrente());
+        }
+    }
+}
