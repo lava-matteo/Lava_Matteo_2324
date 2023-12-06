cea5ddde0bd3b9f482add4fb34d64cd9c34075bc
lava.matteo <lava.matteo.06@itisdalmine.edu.it>
2023-12-06 22:21:48 +0100

    aggiunti i compiti per il 07-12-23

---
+++ 7_12/Temperature/Temperature/Temperature.cs
@@ -0,0 +1,35 @@
+﻿namespace Temperature
+{
+    public class Temperature
+    {
+        public static float CelsiusToFahrenheit(float tempCelsius)
+        {
+            return (tempCelsius * 9 / 5) + 32;
+        }
+
+        public static float FahrenheitToCelsius(float tempFahrenheit)
+        {
+            return (tempFahrenheit - 32) * 5 / 9;
+        }
+
+        public static float CelsiusToKelvin(float tempCelsius)
+        {
+            return tempCelsius + 273.15f;
+        }
+
+        public static float KelvinToCelsius(float tempKelvin)
+        {
+            return tempKelvin - 273.15f;
+        }
+
+        public static float KelvinToFahrenheit(float tempKelvin)
+        {
+            return ((tempKelvin - 273.15f) * 9 / 5) + 32;
+        }
+
+        public static float FahrenheitToKelvin(float tempFahrenheit)
+        {
+            return ((tempFahrenheit - 32) * 5 / 9) + 273.15f;
+        }
+    }
+}
